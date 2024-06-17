b58f46066d8bc7bc52eac73bb10fe6b961c43f26
Thaparoshan143 <101375593+Thaparoshan143@users.noreply.github.com>
2024-06-17 09:19:03 +0545

    add: Log/Debugger

---
+++ Core/Base/Log.hpp
@@ -0,0 +1,75 @@
+#pragma once
+
+#include"./Types.h"
+
+namespace Log
+{
+    typedef enum LogType
+    {
+        TERMINAL = 0, 
+        FILE_OVERWRITE = 1,
+        FILE_APPEND = 2,
+        TERMINAL_AND_FILE_OVERWRITE = 3,
+        TERMINAL_AND_FILE_APPEND = 4,
+    }LogType;
+
+    typedef enum LogDetail
+    {
+        // Just the required text
+        BASIC = 0,
+        TIME_STAMP = 1,
+    };
+
+    class Debug
+    {
+        public:
+        Debug();
+
+        void SetLogFile(String logFileName)
+        {
+            m_logFileName = logFileName;
+        }
+
+        void LogOut()
+        {
+            switch(m_logType)
+            {
+                case TERMINAL:
+                terminalLog();
+                break;
+                case FILE_APPEND:
+                fileLog(std::ios_base::app);
+                break;
+                case FILE_OVERWRITE:
+                fileLog(std::ios_base::out);
+                break;
+                case TERMINAL_AND_FILE_APPEND:
+                terminalLog();
+                fileLog(std::ios_base::app);
+                case TERMINAL_AND_FILE_OVERWRITE:
+                terminalLog();
+                fileLog(std::ios_base::out);
+                break;
+                default:
+                std::cout << "Cannot find the required type" << std::endl;
+                break;
+            }
+        }
+
+        protected:
+        String m_logFileName;
+        LogType m_logType;
+        LogDetail m_logDetail;
+        String m_logBuffer;
+
+        void terminalLog()
+        {
+            
+        }
+
+        void fileLog(uint mode)
+        {
+
+        }
+    };
+}
