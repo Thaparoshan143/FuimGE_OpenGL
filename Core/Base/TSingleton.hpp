bdeccfd4f21c638bb483af39a6051a837c4cec3b
Thaparoshan143 <101375593+Thaparoshan143@users.noreply.github.com>
2024-06-17 08:58:46 +0545

    add: Singleton Templated Interface

---
+++ Core/Base/TSingleton.hpp
@@ -0,0 +1,33 @@
+#pragma once
+
+#include"./Types.h"
+
+namespace Singleton
+{   
+    template<class T>
+    class TISingleton
+    {
+        protected:
+        TISingleton(T *parent)
+        {
+            if(parent != nullptr)
+            {
+                m_instance = dynamic_cast<T*>(parent);
+            }
+            else
+            {
+                std::cout << "Instance can't be null" << std::endl;
+            }
+        }
+
+        virtual ~TISingleton()  {   }
+
+        static const T* GetInstance()    {   return m_instance;  }
+
+        protected:
+        static T *m_instance;
+    };
+
+    template<class T>
+    T* TISingleton<T>::m_instance = nullptr;
+}
