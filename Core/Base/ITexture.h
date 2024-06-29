c7759b5225a3ce4e970937ac7221b0e3e7a93a1f
Thaparoshan143 <101375593+Thaparoshan143@users.noreply.github.com>
2024-06-29 20:24:39 +0545

    add: Type methods (prev type change to target)

--- Core/Base/ITexture.h
+++ Core/Base/ITexture.h
@@ -12,18 +12,24 @@ namespace Interface
         virtual uint Bind() = 0;
         virtual void Unbind() = 0;
         virtual void Offload() = 0;
-        virtual void SetTextureProp(uint texChannel, uint wrapMode, uint texType)
+        virtual void SetTextureProp(uint texChannel, uint wrapMode, uint texTarget)
         {
-            m_channel = texChannel;  m_wrapMode = wrapMode;  m_type = texType;
+            m_channel = texChannel;  m_wrapMode = wrapMode;  m_target = texTarget;
         }
         virtual uchar* loadTexture() = 0;
+        void SetType(String type)   {   m_type = type;  }
+        String& GetType()   {   return m_type;  }
+        bool IsOfType(String check) const  {   return m_type == check;   }
+        void SetSlot(uint slot)  {   m_slot = slot;  }
 
         // contains the handle to texture buffer
-        // type of texture like 1d, 2d, 3d...
+        // target of texture like 1d, 2d, 3d...
         // Number of channel to store in buffer
         // How to wrap in (U,V .) or (S, T .) mode
-        uint m_textureHandle, m_type, m_channel, m_wrapMode;
+        // slot is texture number to activate/refer to
+        uint m_textureHandle, m_target, m_channel, m_wrapMode, m_slot;
         String m_path;
+        String m_type;
         iVec2 m_imgDim;
         uchar *m_buffer;
     };
