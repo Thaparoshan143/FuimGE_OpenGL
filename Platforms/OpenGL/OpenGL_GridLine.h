fe1a4fc677201aa7e8faac664ab0a5050241bb93
Thaparoshan143 <101375593+Thaparoshan143@users.noreply.github.com>
2024-07-16 09:23:09 +0545

    update: properties shader, target and color in GridLine

--- Platforms/OpenGL/OpenGL_GridLine.h
+++ Platforms/OpenGL/OpenGL_GridLine.h
@@ -4,6 +4,8 @@
 #include"../../Core/Base/IEntity.h"
 
 #include"./OpenGL_Buffer.h"
+#include"./OpenGL_Window.h"
+#include"./OpenGL_Shader.h"
 
 #define TESTMODE
 #include"../../Util/Util.cpp"
@@ -20,29 +22,39 @@ namespace OpenGL
         }
 
         fVec2 start, end, gridCount;
+        Color color;
     };
 
     class GridLine : public Interface::IEntity
     {
         public:
-        GridLine(GridProp gridProp) : m_gridProp(gridProp), m_VAO(PPP)
+        GridLine(Interface::IWindow *target, GridProp gridProp) : m_VAO(PPP), m_shader("../res/Shaders/Camera/", "ver.shader", "frag.shader"), m_gridProp(gridProp)
         {
+            m_target = target;
             float *temp = get_grid_pointxy_ind(m_gridProp.start, m_gridProp.end, m_gridProp.gridCount);
             m_VBO.AppendBuffer(temp, get_grid_xy_size(m_gridProp.gridCount));
             m_VBO.Offload();
             m_VAO.AppendBufferLink(&m_VBO);
             m_VAO.EnableVertexAttrib();
+            m_shader.CompileProgram();
         }
 
         void Draw()
         {
             m_VAO.Bind();
+            m_shader.UseProgram();
+            m_shader.SetUniformMat4("view", m_target->GetCamera()->GetViewMatrix());
+            m_shader.SetUniformMat4("projection", m_target->GetCamera()->GetProjectionMatrix(m_target->GetWindowDim()));
+            m_shader.SetUniformMat4("model", m_target->GetCamera()->GetModelMatrix());
+            m_shader.SetUniformVec4("col", m_gridProp.color);
             glDrawArrays(GL_LINES, 0, m_VBO.m_buffer.size());
         }
 
         VertexArrayObject m_VAO;
         VertexBufferObject m_VBO;
+        OpenGL_Shader m_shader;
 
         GridProp m_gridProp;
+        Interface::IWindow *m_target;
     };
 }
