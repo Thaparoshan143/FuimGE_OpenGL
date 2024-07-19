290f839134037f1b0af50e63aac1dbb657170b1d
Thaparoshan143 <101375593+Thaparoshan143@users.noreply.github.com>
2024-07-19 20:13:31 +0545

    update: bash exe directory check before build

--- build.sh
+++ build.sh
@@ -1,2 +1,3 @@
 #!/bin/bash
+[ ! -d "./exe" ] && mkdir exe
 g++ --std=c++14 ./src/glad.cpp ./src/main.cpp -Wall -I ./src -I ./src/components -I ./src/external -I ./includes -L. ./lib/mac/libglfw.3.3.dylib -o ./exe/test -framework OpenGL
