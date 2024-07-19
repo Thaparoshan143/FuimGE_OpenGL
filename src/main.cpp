41f21ea286074e17514d6b3767518bfc2e106910
Thaparoshan143 <101375593+Thaparoshan143@users.noreply.github.com>
2024-07-19 22:48:10 +0545

    update: few test integration

--- src/main.cpp
+++ src/main.cpp
@@ -20,7 +20,7 @@ class GRApplication : public Component::Application
 		glEnable(GL_BLEND);   
 		glEnable(GL_CULL_FACE);
 		glCullFace(GL_FRONT);
-		// glPolygonMode(GL_FRONT_AND_BACK, GL_LINE);
+		glPolygonMode(GL_FRONT_AND_BACK, GL_LINE);
 	}
 
 	void Loop() override
@@ -31,48 +31,25 @@ class GRApplication : public Component::Application
 		Component::Shader shader("./res/Shaders/Basic/", "ver.shader", "frag.shader");
 		shader.CompileProgram();
 
-		Math::Transform transform;
-		transform.position = fVec3(0, 0);
-		transform.rotation = fVec3(0, 0);
-		transform.scale = fVec3(0.5);
-		Color4 col(1, 0, 0.2, 1);
-		Component::Triangle triangle(transform, col);
-		triangle.Offload();
-
-		External::fastObjMesh *mesh = External::fast_obj_read("./res/Models/cube.obj");
-
-		float *position = mesh->positions;
-		std::cout << "Position count : " << mesh->position_count << std::endl;
-		Util::print_float_xyz(position, mesh->position_count*3);
-		std::cout << "------------------------------- -------------------------------" << std::endl;
-		// float *normal = mesh->normals;
-		// std::cout << "Normal count : " << mesh->normal_count << std::endl;
-		// Util::print_float_xyz(normal, mesh->normal_count*3);
-		// std::cout << "------------------------------- -------------------------------" << std::endl;
-		// float *texcoord = mesh->texcoords;
-		// std::cout << "tex coord count : " << mesh->texcoord_count << std::endl;
-		// Util::print_float_xy(texcoord, mesh->texcoord_count*2);
-		// std::cout << "------------------------------- -------------------------------" << std::endl;
-
-		// float *color = mesh->colors;
-		// std::cout << "Color count : " << mesh->color_count << std::endl;
-		// Util::print_float_xyz(color, mesh->color_count*3);
-		// std::cout << "------------------------------- -------------------------------" << std::endl;
-		
-		External::fastObjIndex *index = mesh->indices;
-		std::cout << "Index : " << mesh->index_count*3 << std::endl;
-		for(int i=0;i<mesh->index_count;i++)
-		{
-			std::cout << std::setprecision(5) << (*(index + i)).p << ",\t" << (*(index + i)).t << ",\t" << (*(index + i)).n << std::endl;
-		}
-		std::cout << "------------------------------- -------------------------------" << std::endl;
+		External::FastObjWrapper tempObj("./res/Models/cube.obj");
 
 		vao.Bind();
-		vbo.AppendBuffer(position, mesh->position_count*3);
+		std::vector<float> flattenVert = tempObj.GetVertexPosition();
+		std::cout << "Position Count : " << flattenVert.size() << std::endl;
+		Util::print_float_xyz(flattenVert.data(), flattenVert.size());
+		std::cout << "------------------------------- -------------------------------" << std::endl;
+		vbo.AppendBuffer(flattenVert);
 		vbo.Offload();
 
-		uint32_t *flattenIndex = (uint32_t*)index;
-		ibo.AppendBuffer(flattenIndex, mesh->index_count*3);
+		std::vector<uint32_t> flattenIndex = tempObj.GetIndex();
+		std::cout << "Index : " << flattenIndex.size() << std::endl;
+		for(int i=0;i<flattenIndex.size();i+=3)
+		{
+			std::cout << std::setprecision(5) << *(flattenIndex.data() + i) << ",\t" << *(flattenIndex.data() + i + 1) << ",\t" << *(flattenIndex.data() + i + 2) << std::endl;
+		}
+		std::cout << "------------------------------- -------------------------------" << std::endl;
+		std::cout << "Index Count : " << flattenIndex.size() << std::endl;
+		ibo.AppendBuffer(flattenIndex);
 		ibo.Offload();
 		vao.EnableVertexAttrib();
 
@@ -80,10 +57,9 @@ class GRApplication : public Component::Application
 		{
 			m_window.SetBgColor(DEFAULT_WINDOW_BG);
 			
-			// triangle.Render();
 			vao.Bind();
 			shader.UseProgram();
-			glDrawElements(GL_TRIANGLES, mesh->index_count*3, GL_UNSIGNED_INT, 0);
+			glDrawElements(GL_TRIANGLES, 100, GL_UNSIGNED_INT, 0);
 	
 			m_window.SwapFrameBuffer();
 			delay();
