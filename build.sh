c17fbd869bcdfddc1fb2e33568d8f14901db53f3
Thaparoshan143 <101375593+Thaparoshan143@users.noreply.github.com>
2024-07-20 09:12:31 +0545

    update: compilation command for iamgui and automatic src file of external via find

--- build.sh
+++ build.sh
@@ -1,3 +1,13 @@
 #!/bin/bash
 [ ! -d "./exe" ] && mkdir exe
-g++ --std=c++14 ./src/glad.cpp ./src/main.cpp -Wall -I ./src -I ./src/components -I ./src/external -I ./includes -L. ./lib/mac/libglfw.3.3.dylib -o ./exe/test -framework OpenGL
+
+# Files in src folder inside external (indirect)
+SRC_FILES=$(find ./src/external -type f -name "*.cpp" | sort -u | tr '\n' ' ')
+# ALL_FILES=$(find . -type f -name "*.cpp" -o -name "*.h" | sort -u | tr '\n' ' ')
+INCLUDE_FOLDER= ``
+
+# SRC_FILE=$(ls ./src/external/imgui/ | grep \.cpp)
+# echo "$SRC_FILES"
+# echo "$ALL_FILES"
+# exit 0
+g++ --std=c++14 ./src/glad.cpp ./src/main.cpp $SRC_FILES -Wall -I ./src -I ./src/components -I ./src/external/imgui/backend/ -I ./src/external/imgui/ -I ./includes -L. ./lib/mac/libglfw.3.3.dylib -o ./exe/test -framework OpenGL
