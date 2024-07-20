6d6200fcd1691375540751dd0d32913b6dae7087
Thaparoshan143 <101375593+Thaparoshan143@users.noreply.github.com>
2024-07-20 18:48:14 +0545

    update: operator overloading, qualifier and other fix

--- src/components/structure.cpp
+++ src/components/structure.cpp
@@ -7,9 +7,9 @@ template<typename T>
 struct Vec2
 {
     public:
-    Vec2(T val)   {    x = val;   y = val;   }
+    Vec2(T val = 1)   {    x = val;   y = val;   }
     Vec2(T _x, T _y)   {   x = _x; y = _y; }
-    Vec2(Vec2 &rhs)   {   x = rhs.x;  y = rhs.y; }
+    Vec2(const Vec2 &rhs)   {   x = rhs.x;  y = rhs.y; }
 
     Vec2& operator=(const Vec2 &rhs)
     {
@@ -28,14 +28,30 @@ template<typename T>
 struct Vec3
 {
     public:
-    Vec3(T val)   {  x = val;   y = val;   z = val;   }
+    Vec3(T val = 1)   {  x = val;   y = val;   z = val;   }
     Vec3(T _x, T _y, T _z = 1)   {  x = _x; y = _y; z = _z; }
-    Vec3(Vec3 &rhs)   {  x = rhs.x;  y = rhs.y;  z = rhs.z;; } 
+    Vec3(const Vec3 &rhs)   {  x = rhs.x;  y = rhs.y;  z = rhs.z;; } 
 
     Vec3& operator=(const Vec3 &rhs)
     {
         x = rhs.x;  y = rhs.y;  z = rhs.z;;  
         return *this;
+    }
+
+    friend Vec3 operator*(float v, const Vec3 &rhs);
+    Vec3 operator*(float v)
+    {
+        return Vec3(x*v, y*v, z*v);
+    }
+
+    Vec3 operator-(const Vec3 &rhs)
+    {
+        return Vec3(x-rhs.x, y-rhs.y, z-rhs.z);
+    }
+
+    Vec3 operator+(const Vec3 &rhs)
+    {
+        return Vec3(x+rhs.x, y+rhs.y, z+rhs.z);
     }
 
     union
@@ -46,12 +62,18 @@ struct Vec3
 };
 
 template<typename T>
+Vec3<T> operator*(float v, const Vec3<T> &rhs)
+{
+    return Vec3<T>(rhs.x*v, rhs.y*v, rhs.z*v);
+}
+
+template<typename T>
 struct Vec4
 {
     public:
-    Vec4(T val)   {  x = val;   y = val;   z = val;   w = val;   }
+    Vec4(T val = 1)   {  x = val;   y = val;   z = val;   w = val;   }
     Vec4(T _x, T _y, T _z = 1, T _w = 1)   {  x = _x; y = _y; z = _z; w = _w; }
-    Vec4(Vec4 &rhs)   {   x = rhs.x;  y = rhs.y;  z = rhs.z;  w = rhs.w;  } 
+    Vec4(const Vec4 &rhs)   {   x = rhs.x;  y = rhs.y;  z = rhs.z;  w = rhs.w;  } 
 
     Vec4& operator=(const Vec4 &rhs)
     {
