b496a3e28d53a4ebd08c06b474924ebf2451aaa6
Thaparoshan143 <101375593+Thaparoshan143@users.noreply.github.com>
2024-07-24 08:45:36 +0545

    update: imgui obj based compilation during build

--- build.sh
+++ build.sh
@@ -2,12 +2,8 @@
 [ ! -d "./exe" ] && mkdir exe
 
 # Files in src folder inside external (indirect)
-SRC_FILES=$(find ./src/external -type f -name "*.cpp" | sort -u | tr '\n' ' ')
+IMGUI_FILES=$(find ./src/external -type f -name "*.o" | sort -u | tr '\n' ' ')
+# IMGUI_FILES=$(find ./src/external -type f -name "*.cpp" | sort -u | tr '\n' ' ')
 # ALL_FILES=$(find . -type f -name "*.cpp" -o -name "*.h" | sort -u | tr '\n' ' ')
-INCLUDE_FOLDER= ``
 
-# SRC_FILE=$(ls ./src/external/imgui/ | grep \.cpp)
-# echo "$SRC_FILES"
-# echo "$ALL_FILES"
-# exit 0
-g++ --std=c++14 ./src/glad.cpp ./src/main.cpp $SRC_FILES -Wall -I ./src -I ./src/components -I ./src/external/imgui/backend/ -I ./src/external/imgui/ -I ./includes -L. ./lib/mac/libglfw.3.3.dylib -o ./exe/test -framework OpenGL
+g++ --std=c++14 ./src/glad.cpp ./src/main.cpp $IMGUI_FILES -Wall -I ./src -I ./src/components -I ./src/external/imgui/backend/ -I ./src/external/imgui/ -I ./includes -L. ./lib/mac/libglfw.3.3.dylib -o ./exe/test -framework OpenGL
