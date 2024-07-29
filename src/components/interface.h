772756aac6c44679d5994db36b43fb9f0afec38d
Thaparoshan143 <101375593+Thaparoshan143@users.noreply.github.com>
2024-07-29 07:45:20 +0545

    add: MaterialProp & Entity interface functions

--- src/components/interface.h
+++ src/components/interface.h
@@ -3,6 +3,19 @@
 #include<math.cpp>
 #include<vector>
 
+namespace Component
+{
+    struct MaterialProp
+    {
+        public: 
+        MaterialProp(Color4 col = Color4(0.5, 0.5, 0.5), float spe = 0.4, float diff = 0.2, float amb = 0.1, float rough = 0.5)
+        {
+            color = col;    specular = spe; diffuse = diff; ambient = amb;  roughness = rough;  
+        }
+        Color4 color;
+        float specular, diffuse, ambient, roughness;
+    };
+}
 
 namespace Interface
 {
@@ -10,8 +23,16 @@ namespace Interface
     {
         public: 
         IRenderableEntity() {   }
+        virtual void Update() = 0;
         virtual void Render() = 0;
-        
+        virtual uint32_t GetBufferLayout() = 0;
+        inline std::string& GetName()  {   return name;  }
+        fVec3& GetPosition()    {   return transform.position;  }
+        fVec3& GetRotation()    {   return transform.rotation;  }
+        fVec3& GetScale()    {   return transform.scale;  }
+        void SetTransform(fVec3 pos, fVec3 rot, fVec3 sca)  {   transform.position = pos;   transform.rotation = rot;   transform.scale = sca;  }
+
+        Component::MaterialProp material;        
         protected:
         std::string name;
         bool isActive;
