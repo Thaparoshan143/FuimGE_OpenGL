fe5c834842e41721d927f2efe66259f9b6b63742
Thaparoshan143 <101375593+Thaparoshan143@users.noreply.github.com>
2024-08-01 10:23:48 +0545

    update: GUI open by default

--- src/components/gui.cpp
+++ src/components/gui.cpp
@@ -79,8 +79,8 @@ namespace GUI
         public:
         GUIManager()
         {
-            m_showInspector = false;
-            m_showOutliner = false;
+            m_showInspector = true;
+            m_showOutliner = true;
             m_activeObject = nullptr;
         }
 
